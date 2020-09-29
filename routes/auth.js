ea48b4bb6ddd8fb5046909434206874af4f3160b
Swaraj Rajpure <swarajrajpure@gmail.com>
2020-09-29 20:13:13 +0530

    encrypted password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require('../model/User');
 const { loginValidation, registerValidation } = require('../validation');
 
@@ -7,8 +8,7 @@ const { loginValidation, registerValidation } = require('../validation');
 router.post('/login', async (req,res) => {
     const { error } = loginValidation(req.body);
     if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
     
 })
@@ -16,30 +16,31 @@ router.post('/login', async (req,res) => {
 router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body);
     if(error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     const emailExists = await User.findOne({ email: req.body.email});
     if (emailExists){ 
-        res.status(400).send("Email already exists");
-        return;
+        return res.status(400).send("Email already exists");
     };
 
     const nameExists = await User.findOne({ name: req.body.name});
     if (nameExists) { 
-        res.status(400).send("Name already exists");
-        return;
-};
+        return res.status(400).send("Name already exists");
+    };
+
+    // Hash password
+    const hash = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, hash);
 
     const user = new User( {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send(savedUser._id);
     }
     catch(err) {
         res.status(400).send(err);   
