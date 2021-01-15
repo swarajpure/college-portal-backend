1306c93f3516aa2ef9ec6eaf9af706692b631b83
Swaraj Rajpure <swarajrajpure@gmail.com>
2021-01-15 15:27:40 +0530

    removes role for login and length of name and email

--- middlewares/validators/userValidator.js
+++ middlewares/validators/userValidator.js
@@ -3,8 +3,8 @@ const Joi = require('@hapi/joi');
 // validation schema
 const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().min(6).required(),
     role: Joi.string().required()
   });
@@ -13,9 +13,8 @@ const registerValidation = (data) => {
 
 const loginValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
-    role: Joi.string().required()
+    email: Joi.string().required().email(),
+    password: Joi.string().min(6).required()
   });
   return schema.validate(data);
 };
